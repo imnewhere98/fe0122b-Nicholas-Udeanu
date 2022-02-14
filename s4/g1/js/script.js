050e939af7c2b90e0fda3a62db69adb753f71118
imnewhere98 <nicholas.udeanu@homtail.com>
2022-02-14 17:50:59 +0100

    javag1

---
+++ s4/g1/js/script.js
@@ -0,0 +1,25 @@
+function changeColor() {
+    document.getElementById('colore').style.color ='#FF0000';
+}
+
+function somma( ){
+return    12+4;
+}
+
+window.alert(5 + 6);{
+    
+}
+
+console.log("Hello World!");{
+    var x = 10;
+    var y = 20;
+    var sum = x + y;
+    console.log(sum)
+}
+
+document.write(11 + 6);{
+}
+
+function button(){
+    document.getElementById('myImage').src='pic_bullbon.gif';
+}
